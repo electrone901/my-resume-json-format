de920b853f4f2579f52732f15150a5cbfe84e947
electrone901 <carbajalluis@rocketmail.com>
2019-02-20 16:53:36 -0500

    add resume json format

---
+++ resume.js
@@ -0,0 +1,318 @@
+{
+	firstname: "Luis",
+	lastname: "Carbajal",
+
+	contact: {
+		email: "luis.carbajal32@gmail.com",
+		phone: "(646) 360 - 0420",
+		website: "https://electrone901.github.io",
+		linkedin: "linkedin.com/in/luis-carbajal",
+		github: "github.com/electrone901"
+	},
+
+	skills: {
+		details: [
+		
+			{
+				type: "Mobile Development",
+				items: [
+				"React Native JS"
+				]
+			},
+
+			{	
+				type: "Programming Languages",
+				items: [
+					"JavaScript",
+					"Java",
+					"SQL",
+					"HTML",
+					"Ruby in Rails",
+					"PHP"
+				]
+			},
+
+
+			{
+				type: "Web Frameworks",
+				items: [
+					"React.js",
+					"CSS3",
+					"Express.js",
+					"HTML5",
+					"Node.js",
+					"Sequelize",
+					"FlexBox",
+					"Bootstrap",
+					"Test-driven development (TDD)",
+					"WordPress",
+					"REST",
+					"JSON"
+				]
+			},
+
+
+			{
+				type: "Databases",
+				items: [
+					"MySQL",
+					"PostgreSQL"
+				]
+			},
+
+			{
+				type: "Systems & Tools",
+				items: [
+					"Git",
+					"GitHub",
+					"Linux",
+					"Heroku",
+					"Windows",
+					"HDFS",
+					"Webpack",
+					"Adobe Creative Suite",
+				]
+			},
+
+			{
+			type: "Interests",
+			items: [
+			"Big Data",
+			"Machine Learning"
+			]
+			}
+
+		]
+	},
+
+	github_projects: {
+
+		items: [
+
+			{
+				project_name: "La Zona App",
+				tagline: "[https://github.com/electrone901/laZonaWebsite](http://lazonav2.herokuapp.com/)",
+				
+				description: [
+					"Connects people with businesses, services and products in a quick and easy way within your neighborhood."
+				],
+
+				technology_used: {
+					tools: [
+						"JavaScript",
+						"HandleBars",
+						"Mongo Databases",
+						"Express Server",
+					]
+				}
+			},
+
+
+			{
+				project_name: "Mixteca Inc.",
+				tagline: "[https://github.com/electrone901](http://www.mixteca.org/)",
+				
+				description: [
+					"Responsive web app that connects people by providing them access to services and education."
+				],
+
+				technology_used: {
+					tools: [
+						"JavaScript",
+						"CSS3",
+						"SASS",
+						"WordPress",
+					]
+				}
+			},
+
+
+			{
+				project_name: "EdTrack",
+				tagline: "[https://github.com/electrone901/EdTrack](https://bit.ly/2OKEEon)",
+				
+				description: [
+					"Tracking tool that helps teachers and students to better track and learn from their performance."
+				],
+
+				technology_used: {
+					tools: [
+						"React JS",
+						"JavaScript",
+						"PostgreSQL",
+						"Express Server",
+					]
+				}
+			},
+
+
+			{
+				project_name: "Auto Accessories App ",
+				tagline: "[https://github.com/electrone901/Car-Accessories-App](https://caraccessories.herokuapp.com/)",
+				
+				description: [
+					"Web app that facilitates and simplifies the management of your post by interactively adding, deleting and updating functionality."
+				],
+
+				technology_used: {
+					tools: [
+						"React JS",
+						"JavaScript",
+						"PostgreSQL",
+						"Express Server",
+					]
+				}
+			},
+		]
+	},
+
+	work_experience: {
+		items: [
+			
+			{
+				title: "CUNY College Assistant IT Department",
+				organisation: "City University of New York",
+				location: "Brooklyn, NY",
+				from: "June 2018",
+				to: "Present",
+
+				details: [
+					"Assist personnel with installation, configuration and ongoing usability of system hardware and software"
+				],
+
+				technology_used: {
+					tools: [
+						"PostgreSQL",
+						"Mongo Databases",
+						"Windows, IOS X",
+					]
+				}
+			},
+
+			{
+				title: "CUNY Tech Prep 2018",
+				organisation: "CUNY",
+				location: "New York, NY",
+				from: "June 2017",
+				to: "June 2018",
+
+				details: [
+					"Prepare for a software engineering career via a competitive, yearlong training program that blends a full stack JavaScript boot camp with professional development coaching with a less than 6% acceptance rate. <<https://bit.ly/2NxrFtS>>"
+				],
+
+				technology_used: {
+					tools: [
+						"JavaScript",
+						"React",
+						"PostgreSQL",
+						"SQL"
+					]
+				}
+			},
+
+			{
+				title: "Coalition For Queens",
+				organisation: "C4Q",
+				location: "Queens, NY",
+				from: "June 2016",
+				to: "August 2017",
+
+				details: [
+					"Graduated from a 46-week full-time project-based development program with a less than 4% acceptance rate."
+				],
+
+				technology_used: {
+					tools: [
+						"Data Structures and Algorithms",
+						"React",
+						"React Native",
+						"PostgreSQL",
+						"Firebase",
+						"Mongo Databases"
+					]
+				}
+			}
+
+		]
+	},
+
+	involvement: {
+		organizations: [
+			"Volunteer to build a responsive website with focused on UI for H.A.N.D.S, Fall 2016",
+			"Volunteer to teach html & CSS at Tepeyac Ass. NY, Fall 2017",
+			"Participated at Xignite hackathon and won Xignite API Award Winners",
+			"Active member of IME-BECAS Scholarship Program at Cuny, Fall 2015 - Spring 2018"
+		]
+	},
+
+	achievements: {
+		items: [
+			"I earned a place on the Dean's List my freshman and sophomore year, awarding me for Peter Jennings scholarship and The Magna Cum Laude graduation honors.",
+			"First Place for Most Innovative and Socially Impactful App 2017 by Financial Revolution",
+			"Peter Jennings Laurel Scholarship Student Award & Bank of America 2016",
+			"CUNY Scholarship Award 2016",
+			"Winner of GovTech Challenge at AngelHack Global Hackathon: Manhattan 2015",
+			"Honors Scholar Program: The City College of New York, Brooklyn, NY."
+		]
+	},
+
+	additional_info: {
+		items: [
+		"Languages: English (fluent), Spanish (fluent)",
+		"Interests: Running, Board Games, Mathematics."
+		]
+	},
+
+	education: {
+		schools: [
+			{
+				degree: "Bachelor of Science",
+				major: "Computer Information System",
+				institution: "New York City College of Technology",
+				graduation_year: 2018
+			}
+		]
+	},
+
+	undergrad_courses: [
+		{
+		name: "Data Structures & Algorithm Course",
+		url: "https://www.citytech.cuny.edu/mathematics/docs/courses/MAT2440.pdf"
+		},
+		{
+		name: "Discrete Structures and Algorithms II",
+		url: "https://www.citytech.cuny.edu/mathematics/docs/courses/MAT2540.pdf"
+		},
+		{
+		name: "Object Oriented Programming in Java",
+		url: "https://openlab.citytech.cuny.edu/groups/object-oriented-programming-in-java/"
+		},
+		{
+		name: "Mobile Device and Multimedia Programming",
+		url: "https://openlab.citytech.cuny.edu/groups/cst-2301-mobile-device-and-multimedia-programming-fa2015/"
+		}
+	],
+
+	links: [
+		{
+		name: "Linkedin",
+		url: "https://www.linkedin.com/in/luis-carbajal/"
+		},
+		{
+		name: "Github",
+		url: "https://github.com/electrone901"
+		},
+		{
+		name: "Portfolio",
+		url: "https://electrone901.github.io/"
+		},
+		{
+		name: "Twitter",
+		url: "https://twitter.com/byBetoNY"
+		}
+	]
+}
+
+
+
+
